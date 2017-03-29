77446110583b1857496f27053dca01f55a98bb96
YozhEzhi <yozhezhi@gmail.com>
2017-03-29 18:03:01 +0300

    npm update

--- gulp/utils/register.js
+++ gulp/utils/register.js
@@ -1,7 +1,3 @@
-'use strict';
-
 module.exports = (gulp, plugins, config) => (tasks) => {
-  tasks.forEach(function(task) {
-    gulp.task(task, require(`../${task}`)(gulp, plugins, config));
-  });
+  tasks.forEach(task => gulp.task(task, require(`../${task}`)(gulp, plugins, config)));
 };
